db3372a36bf7350c93cb2ddbd85769586ff7e1c1
Thada Wangthammang <thada.wth@gmail.com>
2024-05-25 15:29:58 +0700

    chore: prep for release

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,1 @@
-import { sum } from './lib';
-
-(async () => {
-  // Top-level await [read more](https://stackoverflow.com/a/46515787)
-  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  console.log(`result: `, await result.json());
-
-  console.log(`1 + 2 = ${sum(1, 2)}`);
-})();
+export * from './lib';
